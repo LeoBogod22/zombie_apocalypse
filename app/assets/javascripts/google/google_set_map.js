bbad987b9c0d7ffd5113384dbac2d7be49756d61
azheng249 <andy.zheng249@gmail.com>
2016-05-05 19:03:43 -0400

    added comments to google maps functions and moved #findAndMapServices call inside an if statement. need to add post requests to create locations and places on the backend of app

--- app/assets/javascripts/google/google_set_map.js
+++ app/assets/javascripts/google/google_set_map.js
@@ -2,13 +2,14 @@ var map;
 var geocoder;
 var infowindow;
 var currentCoords;
+var defaultAddress = "New York, NY";
 
 function milesToMeters (miles) {
-  return miles * 1609;
+  return miles * 1609.34;
 }
 
+// Sets the map to default location then adds listener for input
 function initGoogleMap () {
-  var defaultAddress = "New York, NY";
 
   // Set Google Maps library variables
   geocoder = new google.maps.Geocoder();
@@ -16,39 +17,13 @@ function initGoogleMap () {
 
   map = new google.maps.Map($("#map-canvass")[0], {zoom: 15, center: {lat: -34.397, lng: 150.644}});
 
-  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('pac-location-input'));
+  var autocomplete = new google.maps.places.Autocomplete($('#pac-location-input')[0]);
 
   setGoogleMapLocation(defaultAddress);
   locationInputListener();
 }
 
-function setGoogleMapLocation (address) {
-  // Get geolocation of default address and set map to that location
-  geocoder.geocode({'address': address}, function(results, status){
-    if (status === google.maps.GeocoderStatus.OK) {
-      currentCoords = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
-
-      map.setCenter(results[0].geometry.location);
-      placeMarker(map, results[0]);
-
-    } else {
-      console.log("Something went wrong when trying to set the location: " + status);
-    }
-  });  
-}
-
-function placeMarker (map, place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
+// Listens for client's location input and then resets the map
 function locationInputListener () {
 
   $("#pac-location-form").submit(function(e){
@@ -58,43 +33,80 @@ function locationInputListener () {
     var locationInput = $("#pac-location-input").val();
     setGoogleMapLocation(locationInput);
 
-    // Initialize Google Places Service
-    var services = new google.maps.places.PlacesService(map);
-
-    var types =  [
-      'natural_feature',
-      'convenience_store',
-      'grocery_or_supermarket',
-      'hospital',
-      'pharmacy',
-      'doctor',
-      'hardware_store',
-      'gas_station',
-      'airport',
-      'parking'
-    ];
-
-    for (var i = 0; i < types.length; i++) {
-      services.nearbySearch({
-        location: currentCoords,
-        radius: milesToMeters(1),
-        type: [types[i]]
-      }, mapServicesCallback);      
-    };
-
-
   });    
 
 }
 
+// Takes an address string and searches for geolocation coordinates. 
+// Centers map on coordinates then finds and places markers for select places
+function setGoogleMapLocation (address) {
+  // Get geolocation of default address and set map to that location
+  geocoder.geocode({'address': address}, function(results, status){
+    if (status === google.maps.GeocoderStatus.OK) {
+      currentCoords = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
+
+      map.setCenter(results[0].geometry.location);
+      if (address != defaultAddress){
+        findAndMapServices();
+      }
+    } else {
+      console.log("Something went wrong when trying to set the location: " + status);
+    }
+  });  
+}
+
+// Called from setGoogleMapLocation
+// Loops through a types array place types and calls mapServicesCallback to mark results
+function findAndMapServices () {
+  // Initialize Google Places Service
+  var services = new google.maps.places.PlacesService(map);
+
+  var types =  [
+    'natural_feature',
+    'convenience_store',
+    'grocery_or_supermarket',
+    'hospital',
+    'pharmacy',
+    'doctor',
+    'hardware_store',
+    'gas_station',
+    'airport',
+    'parking'
+  ];
+
+  for (var i = 0; i < types.length; i++) {
+    services.nearbySearch({
+      location: currentCoords,
+      radius: milesToMeters(0.5),
+      type: [types[i]]
+    }, mapServicesCallback);      
+  };
+}
+
+// Takes results from Google PlacesService's nearbySearch call and marks each result on map
 function mapServicesCallback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      console.log(results[i]);
       placeMarker(map, results[i]);
     }
   }
 }
+
+// Places marker for result
+function placeMarker (map, place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name + " - " + place.types.join(", "));
+    infowindow.open(map, this);
+  });
+}
+
+
+
 
 
 
