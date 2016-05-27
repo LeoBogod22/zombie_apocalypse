b07ad2a7ee629306e5d3e8d284061e564349f405
azheng249 <andy.zheng249@gmail.com>
2016-05-27 16:05:55 -0400

    moved ajax call for population density from javascript code to ruby. put it inside the location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -15,4 +15,29 @@ class Location < ActiveRecord::Base
 
     resource_types.map { |resource| places.where(resource_type: resource).inject(0){|sum, r| sum + r.resource_count}}
   end
+
+  # In square kilometers
+  def population_density
+    url = "http://www.datasciencetoolkit.org/coordinates2statistics/" + latitude + "%2c" + longitude + "?statistics=population_density"
+
+    resp = Faraday.get(url)
+    body = JSON.parse(resp.body)
+
+    return body[0]["statistics"]["population_density"]["value"]
+  end
+
+  # def square_miles_to_square_kilometers(square_miles)
+  #   square_miles * 2.58999
+  # end
+  #
+  # def square_kilometers_to_square_miles (square_kilometers)
+  #   square_kilometers * 0.386102
+  # end
+
+  def survival?
+    # In square kilometers, from https://morrisseyweb.com/2012/03/09/the-average-us-population-density/
+    average_population_density = 11011.3345;
+
+    percentage = average_population_density / population_density
+  end
 end
