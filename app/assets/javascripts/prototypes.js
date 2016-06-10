3039f004eefef0e3411f028d179817af4b56aa38
azheng249 <andy.zheng249@gmail.com>
2016-06-10 18:31:46 -0400

    changed swatAt function for Array prototype to not use default values. default parameters are not supported in browsers other than chrome. caused problems when minifying assets

--- app/assets/javascripts/prototypes.js
+++ app/assets/javascripts/prototypes.js
@@ -2,7 +2,11 @@ String.prototype.capitalize = function () {
   return this[0].toUpperCase() + this.slice(1);
 };
 
-Array.prototype.swapAt = function (index1, index2 = this.length - 1) {
+Array.prototype.swapAt = function (index1, index2) {
+  if (index2 === undefined) {
+    index2 = this.length - 1;
+  }
+
   var elementAtIndex1 = this[index1];
   this[index1] = this[index2];
   this[index2] = elementAtIndex1;
